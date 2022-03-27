2c4f836c637dab13be0899a5e103852368b84c50
nurlan199206 <user_1771@mail.ru>
2022-03-27 17:35:18 +0000

    my first commit

---
+++ index.go
@@ -0,0 +1,15 @@
+package main
+import (
+ "fmt"
+ "os"
+)
+func main() {
+ hostname, error := os.Hostname()
+ if error != nil {
+  panic(error)
+ }  
+ fmt.Println("hostname returned from Environment : ", hostname)
+ fmt.Println("error : ", error)
+
+}
+
