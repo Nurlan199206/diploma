d94b3187bf37192606d4a85360434d05ea63957f
Nurlan199206 <nurlan.i@test.kz>
2023-08-14 23:00:08 +0600

    new endpoints

--- index.go
+++ index.go
@@ -15,5 +15,13 @@ func main() {
         fmt.Fprintf(w, "Hello, from pod: %s\n", name)
     })
 
+    http.HandleFunc("/api", func(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprintf(w, "api endpoint", name)
+    })
+
+    http.HandleFunc("/payment", func(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprintf(w, "payment page", name)
+    })
+
     http.ListenAndServe(":8080", nil)
 }
